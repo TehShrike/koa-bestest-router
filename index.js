dbd1f49f5214847a0552b8891d22bc85a036d7ec
Josh Duff <me@JoshDuff.com>
2017-04-08 11:54:55 -0500

    Only declare notFound once

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ module.exports = function createRouter(methodsToRouteMaps, { set404 = false } = 
 	validateInputMap(methodsToRouteMaps)
 	const getRoutesForMethod = processInputMaps(methodsToRouteMaps)
 
+	async function notFound(context, next) {
+		if (set404) {
+			context.status = 404
+		}
+
+		await next()
+	}
+
 	return async function middleware(context, next) {
 		const { method, url } = context
 		const routes = getRoutesForMethod(method)
-
-		async function notFound() {
-			if (set404) {
-				context.status = 404
-			}
-
-			await next()
-		}
 
 		if (routes) {
 			const path = parseUrl(url).pathname
@@ -29,10 +29,10 @@ module.exports = function createRouter(methodsToRouteMaps, { set404 = false } = 
 
 				await handler(context, next)
 			} else {
-				await notFound()
+				await notFound(context, next)
 			}
 		} else {
-			await notFound()
+			await notFound(context, next)
 		}
 	}
 }
